2c24bd995f5c43ac962dc4af8f54602136e92568
lovesh harchandani <lovesh.bond@gmail.com>
2018-09-17 23:29:59 +0530

    more appropriate order
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- src/test-gadget.cpp
+++ src/test-gadget.cpp
@@ -12,60 +12,62 @@
 using namespace libsnark;
 using namespace std;
 
-int main()
-{
-  // Initialize the curve parameters
+int main() {
+    // Initialize the curve parameters
 
-  default_r1cs_ppzksnark_pp::init_public_params();
+    default_r1cs_ppzksnark_pp::init_public_params();
 
-  typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
-  
-  // Create protoboard
+    typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
 
-  protoboard<FieldT> pb;
-  pb_variable<FieldT> out;
-  pb_variable<FieldT> x;
+    // Create protoboard
 
-  // Allocate variables
+    protoboard<FieldT> pb;
+    pb_variable<FieldT> out;
+    pb_variable<FieldT> x;
 
-  out.allocate(pb, "out");
-  x.allocate(pb, "x");
+    // Allocate variables
 
-  // This sets up the protoboard variables
-  // so that the first one (out) represents the public
-  // input and the rest is private input
+    out.allocate(pb, "out");
+    x.allocate(pb, "x");
 
-  pb.set_input_sizes(1);
+    // This sets up the protoboard variables
+    // so that the first one (out) represents the public
+    // input and the rest is private input
 
-  // Initialize gadget
+    pb.set_input_sizes(1);
 
-  test_gadget<FieldT> g(pb, out, x);
-  g.generate_r1cs_constraints();
-  
-  // Add witness values
+    // Initialize gadget
 
-  pb.val(out) = 35;
-  pb.val(x) = 3;
+    test_gadget<FieldT> g(pb, out, x);
+    g.generate_r1cs_constraints();
 
-  g.generate_r1cs_witness();
-  
-  const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
+    // Trusted setup
+    const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
 
-  const r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(constraint_system);
+    const r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(
+            constraint_system);
+    
+    // Add witness values
 
-  const r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
+    pb.val(out) = 35;
+    pb.val(x) = 3;
 
-  bool verified = r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
+    g.generate_r1cs_witness();
 
-  cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
-  cout << "Primary (public) input: " << pb.primary_input() << endl;
-  cout << "Auxiliary (private) input: " << pb.auxiliary_input() << endl;
-  cout << "Verification status: " << verified << endl;
+    const r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(
+            keypair.pk, pb.primary_input(), pb.auxiliary_input());
 
-  const r1cs_ppzksnark_verification_key<default_r1cs_ppzksnark_pp> vk = keypair.vk;
+    bool verified = r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
 
-  print_vk_to_file<default_r1cs_ppzksnark_pp>(vk, "../build/vk_data");
-  print_proof_to_file<default_r1cs_ppzksnark_pp>(proof, "../build/proof_data");
+    cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
+    cout << "Primary (public) input: " << pb.primary_input() << endl;
+    cout << "Auxiliary (private) input: " << pb.auxiliary_input() << endl;
+    cout << "Verification status: " << verified << endl;
 
-  return 0;
+    const r1cs_ppzksnark_verification_key<default_r1cs_ppzksnark_pp> vk = keypair.vk;
+
+    print_vk_to_file<default_r1cs_ppzksnark_pp>(vk, "../build/vk_data");
+    print_proof_to_file<default_r1cs_ppzksnark_pp>(proof, "../build/proof_data");
+
+    return 0;
 }
