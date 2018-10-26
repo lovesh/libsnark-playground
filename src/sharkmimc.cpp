085470f5273d91e60c7418c2be0fa62e0baa4725
lovesh harchandani <lovesh.bond@gmail.com>
2018-10-26 00:18:32 -0600

    add timing info
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- src/sharkmimc.cpp
+++ src/sharkmimc.cpp
@@ -30,16 +30,25 @@ int main() {
     g.prepare_round_keys();
     g.generate_r1cs_constraints();
 
+    cout << "Number of constraints: " << pb.num_constraints() << endl;
+
     // Trusted setup
     const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
 
     const r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(
             constraint_system);
 
+    duration<double> tc(0);
+    duration<double> tp(0);
+    steady_clock::time_point begin = steady_clock::now();
+
     auto i0 = FieldT("21871881226116355513319084168586976250335411806112527735069209751513595455673");
     auto i1 = FieldT("55049861378429053168722197095693172831329974911537953231866155060049976290");
     auto i2 = FieldT("21871881226116355513319084168586976250335411806112527735069209751513595455673");
     auto i3 = FieldT("55049861378429053168722197095693172831329974911537953231866155060049976290");
+
+    steady_clock::time_point mid = steady_clock::now();
+    tc += duration_cast<duration<double>>(mid - begin);
 
     /*input.resize(4);
     input.fill_with_field_elements(pb, field_elems);*/
@@ -52,5 +61,13 @@ int main() {
 
     cout << "Satisfied status: " << pb.is_satisfied() << endl;
 
+    const r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(
+            keypair.pk, pb.primary_input(), pb.auxiliary_input());
+
+    steady_clock::time_point end = steady_clock::now();
+    tp += duration_cast<duration<double>>(end - begin);
+
+    cout << "Total constraint generation time (seconds): " << tc.count() << endl;
+    cout << "Total proving time (seconds): " << tp.count() << endl;
     return 0;
 }
